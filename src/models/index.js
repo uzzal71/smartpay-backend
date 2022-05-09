aa62ef7edac4cfa718be950a8f9b8cd218600126
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-10 00:10:00 +0600

    office Schedule Model Added

--- src/models/index.js
+++ src/models/index.js
@@ -2,12 +2,14 @@ import User from "./userModel";
 import Company from "./companyModel";
 import Department from "./departmentModel";
 import Designation from "./designationModel";
+import Schedule from "./officeScheduleModel";
 
 const models = { 
     User,  
     Company,
     Department,
-    Designation
+    Designation,
+    Schedule
 }
 
 export default models;
---
+++ src/models/officeScheduleModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const officeScheduleSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        office_start: { type: String, required: true },
+        office_end: { type: String, required: true },
+        late_start: { type: String, required: true },
+        status: { type: String, enum: ['active', 'inactive'] },
+        createdAt: { type: Date, required: true }
+    }
+);
+
+const schedule = mongoose.model("Schedule", officeScheduleSchema);
+
+export default schedule;
